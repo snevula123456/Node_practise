2cba334ba2f84d25eca89abdcbdb44f639b5b65f
snevula123456 <snevula123@gmail.com>
2022-01-07 19:59:17 +0530

    Second Prodcts List

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
+
+const axios = require("axios");
+
+const ProductsURL = "https://fakestoreapi.com/products";
 
 const products = [
   {
@@ -21,8 +24,14 @@ const products = [
 // @desc  get the Products
 // @access Public
 
-router.get("/products", (req, res) => {
-  res.json(products);
+router.get("/products", async (req, res) => {
+  try {
+    const productsList = await axios.get(ProductsURL);
+    res.json(productsList.data);
+  } catch (error) {
+    console.log("🚀 ~ file: products.js ~ line 32 ~ router.get ~ error", error);
+    res.status(400);
+  }
 });
 
 module.exports = router;
